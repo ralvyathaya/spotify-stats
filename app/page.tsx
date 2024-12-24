d47818bdbfcbc44e295eb5f6c03c7f3038dfbb25
rxlvx <108793709+ralvyathaya@users.noreply.github.com>
2024-12-24 23:38:54 +0700

    Fix redirect behavior in Home component by changing window.location assignment to window.location.href for improved compatibility.

--- app/page.tsx
+++ app/page.tsx
@@ -23,7 +23,7 @@ export default function Home() {
           window.history.pushState({}, null, '/')
         })
         .catch(() => {
-          window.location = '/'
+          window.location.href = '/'
         })
     }
   }, [])

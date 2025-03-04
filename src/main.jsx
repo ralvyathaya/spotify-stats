c4ecffd9b60df24a98b494c25b737d02d21fd3f0
rxlvx <108793709+ralvyathaya@users.noreply.github.com>
2025-03-04 08:55:59 +0700

    Fix import casing for App component in main entry file

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './app' // Changed to match casing
+import App from './App.jsx' // Changed to match casing
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
